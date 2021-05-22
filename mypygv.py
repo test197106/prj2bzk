2c0dfc9ebe8ec3cc24a74dbaeb8a1556a424bce6
test197106 <76862379+test197106@users.noreply.github.com>
2021-05-22 06:26:50 +0530

    Create mypygv.py

---
+++ mypygv.py
@@ -0,0 +1,8 @@
+def fn_init(i_prjcode):
+# Initialize important variables
+    global st_prjname
+    global st_prjrole
+    global di_dirs
+    st_prjname    = ''
+    st_prjrole    = ''
+    di_dirs = {}
