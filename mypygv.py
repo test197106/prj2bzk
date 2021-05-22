503baf2cabe4f789959359d7ce3d01c044f655a5
test197106 <76862379+test197106@users.noreply.github.com>
2021-05-22 10:53:50 +0530

    Update mypygv.py

--- mypygv.py
+++ mypygv.py
@@ -3,6 +3,26 @@ def fn_init():
     global st_prjname
     global st_prjrole
     global di_dirs
-    st_prjname    = ''
-    st_prjrole    = ''
-    di_dirs = {}
+    try:
+        x=type(st_prjname)
+    except:
+        st_prjname = None
+    try:
+        x=type(st_prjrole)
+    except:
+        st_prjrole = None
+    try:
+        x=type(di_dirs)
+    except:
+        di_dirs={}
+        di_dirs['code_dir_01'] = None
+        di_dirs['data_dir_01'] = None
+        di_dirs['data_dir_02'] = None
+        di_dirs['data_dir_03'] = None
+        di_dirs['data_dir_04'] = None
+        di_dirs['data_dir_05'] = None
+        di_dirs['data_dir_06'] = None
+        di_dirs['data_dir_07'] = None
+        di_dirs['data_dir_08'] = None
+        di_dirs['data_dir_09'] = None
+        di_dirs['data_dir_10'] = None
