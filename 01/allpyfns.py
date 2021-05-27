72c6bdd12d820afc53b3e046913856c2fb3e2315
test197106 <76862379+test197106@users.noreply.github.com>
2021-05-27 17:57:14 +0530

    Add files via upload

--- 01/allpyfns.py
+++ 01/allpyfns.py
@@ -1,7 +1,6 @@
-
-
 import os
 import sys
+import importlib
 import platform
 import sqlite3 as sq3
 import pandas as pd
@@ -16,23 +15,34 @@ import uuid
 #import requests
 #import json
 #import re
-import importlib
 import mypygvar as gv
-
-
+def fn_get_functions():
+    e = '=================================================================================='
+    print(e+'\nFunctions of module: gv\n'+e)
+    for x in sorted(globals()):
+        if x.startswith('fn_'):
+            print('gv.'+x)
+    print(e,end='\n\n')
+def fn_get_vars():
+    m = [ 'sys', 'Path', 'importlib' ]
+    #m = [ 'requests', 'json', 're'  ]
+    e = '=================================================================================='
+    print(e+'\nVariables of module: gv\n'+e)
+    for x in sorted(globals()):
+        if x not in m and not x.startswith('_') and not x.startswith('fn_'):
+            print('gv.'+x)
+    print(e,end='\n\n')
+def fn_ping():
+    print('I am alive!')
 def fn_reload():
     x=importlib.reload(sys.modules['allpyfns'])
     y=str(x).replace('>','').replace("'","").split()[3]
     print('Reloaded functions from {}'.format(y))
-
-
 def fn_cr_sq3exe(i_4lr, i_exefname, i_exefile):
     ft_now=datetime.now()
     li_f=['p1s0', 0, 0, 1, i_4lr, 'sq3ex', i_exefname, 0, 0.0, True, ft_now, i_exefile ]
     j=len(df_files.index)
     df_files.loc[j] = li_f
-
-
 def fn_cr_sq3inp(i_4lr, i_p, i_s, no_e, no_t):
     ft_now=datetime.now()
     i_e = 'e' + str(no_e)
@@ -45,8 +55,6 @@ def fn_cr_sq3inp(i_4lr, i_p, i_s, no_e, no_t):
     li_f=[fs_psn, 0, no_e, no_t, fs_en, 'sq3in', fs_fn, 0, 0.0, True, ft_now, fo_ff ]
     j=len(df_files.index)
     df_files.loc[j] = li_f
-
-
 def fn_cr_sq3out(i_4lr, i_p, i_s, no_e, no_t):
     ft_now=datetime.now()
     i_e = 'e' + str(no_e)
@@ -59,8 +67,6 @@ def fn_cr_sq3out(i_4lr, i_p, i_s, no_e, no_t):
     li_f=[fs_psn, 0, no_e, no_t, fs_en, 'sq3ou', fs_fn, 0, 0.0, True, ft_now, fo_ff ]
     j=len(df_files.index)
     df_files.loc[j] = li_f
-
-
 def fn_setup_sqlite3():
     # Find the executable path name for sqlite3 exec file
     fv_out=platform.system()
@@ -73,8 +79,6 @@ def fn_setup_sqlite3():
     print(fo_exefile)
     fn_cr_sq3inp('inp0', 'p1', 's0', 0, 1)
     fn_cr_sq3out('out0', 'p1', 's0', 0, 1)
-
-
 def fn_create_di_dbs():
     global di_dbs
     di_dbs = {}
@@ -87,8 +91,6 @@ def fn_create_di_dbs():
         p = k + ' : ' + di_dbs.get(k)
         print(p)
     print('di_dbs recreated')
-
-
 def fn_create_sq3dbf(i_3lr, i_p, i_s):
     global di_dbs
     ft_now=datetime.now()
@@ -104,14 +106,11 @@ def fn_create_sq3dbf(i_3lr, i_p, i_s):
         pass
     print('Updating dictionary')
     fn_create_di_dbs()
-
-
 # Checkpoint important information that might be needed after a cold restart
 def do_ckpt_type_a():
     global df_files
     global df_events    
     fv_uuid=str(uuid.uuid4()).replace('-', '')
-    
     # Saving df_files
     po_hsd_df_files  = po_hisbase / 'saved' / 'df_files'
     po_hsd_df_files.mkdir(mode=0o755, parents=True, exist_ok=True)
@@ -121,7 +120,6 @@ def do_ckpt_type_a():
         print('\nCopying {}\n     to {}'.format(po_fls_scv,po_his_df_files))
     except:
         pass
-    
     # Saving df_events
     po_hsd_df_events = po_hisbase / 'saved' / 'df_events'
     po_hsd_df_events.mkdir(mode=0o755, parents=True, exist_ok=True)
@@ -131,8 +129,6 @@ def do_ckpt_type_a():
         print('\nCopying {}\n     to {}'.format(po_eve_scv,po_his_df_events))
     except:
         pass
-
-
 # Create df_events dataframe for stroring time records of events in this project
 def fn_it_df_events():
     global df_events
@@ -152,8 +148,6 @@ def fn_log_event(i_fn, i_fn_det, i_bt, i_et, i_msg, i_rc):
     df_events.to_csv(po_eve_scv, header=True, index=False)
     print('{} {} in {} seconds'.format(df_events.loc[j,'status'],df_events.loc[j,'e_remarks'],
                             df_events.loc[j,'ela_secs']))
-
-
 def fn_cr_df_events():
     #fn_it_df_events()
     global df_events
@@ -166,8 +160,6 @@ def fn_cr_df_events():
     fn_cc_df_events()
     print(df_events.info())
     #print(df_events)
-
-
 def fn_restart_df_files():
     global df_files
     global cf_files_r
@@ -180,8 +172,6 @@ def fn_restart_df_files():
     print('\n\nShowing LONG columns:\n')   
     for i in df_files.index:
         print( str(df_files.iloc[i,1]) + ' ' + str(df_files.iloc[i,11]) )
-
-
 def fn_load_df_files(i_inpfile):
     global df_files
     global cf_files_r
@@ -194,16 +184,12 @@ def fn_load_df_files(i_inpfile):
     print('\n\nShowing LONG columns:\n')   
     for i in df_files.index:
         print( str(df_files.iloc[i,1]) + ' ' + str(df_files.iloc[i,11]) )
-
-
 # Create df_files dataframe for stroring metadata about files processed
 def fn_it_df_files():
     global df_files
     global cf_files
     global cf_files_r
     df_files = pd.DataFrame(columns=cf_files)
-
-
 def fn_cc_df_files():
     df_files['ps_name'] = df_files['ps_name'].astype('string')
     df_files['f_id'] = df_files['f_id'].astype('int')
@@ -216,8 +202,6 @@ def fn_cc_df_files():
     df_files['s_pct'] = df_files['s_pct'].astype('float')
     df_files['isValid'] = df_files['isValid'].astype('bool')
     df_files['f_modified'] = df_files['f_modified'].astype('datetime64[ns]')
-
-
 def fn_show_df_files(i_ps_name):
     df_files.to_csv(po_fls_scv, header=True, index=False)
     df_files.to_csv(po_fls_bkp, header=True, index=False)
@@ -228,8 +212,6 @@ def fn_show_df_files(i_ps_name):
     print('\n\nShowing LONG columns:\n')   
     for i in tdf_temp1.index:
         print( str(df_files.iloc[i,1]) + ' ' + str(df_files.iloc[i,11]) )
-
-
 def fn_cr_df_files():
     if po_fls_scv.exists():
         fv_uuid=str(uuid.uuid4()).replace('-', '')
@@ -242,8 +224,6 @@ def fn_cr_df_files():
     fn_cc_df_files()
     print(df_files.info())
     print(df_files)
-
-
 def fn_closing_event(i_ps, i_inc_di_dbs='Y'):
     global df_files
     global di_dbs
@@ -257,8 +237,6 @@ def fn_closing_event(i_ps, i_inc_di_dbs='Y'):
     di_dbs={}
     print(df_files)
     print(di_dbs)
-
-
 def fn_imptbl_info(i_inc_di_dbs='Y'):
     print(df_files.info())
     print(df_events.info())
@@ -268,12 +246,8 @@ def fn_imptbl_info(i_inc_di_dbs='Y'):
     if i_inc_di_dbs == 'Y':
         print('Contents of di_dbs')
         print(di_dbs)
-
-
 def fn_p1s0_tasks():
-    # Predefined Variables. Please do not edit the values in this cell
     f_btime_o=datetime.now()
-
     global cf_events
     global df_events
     global cf_files
@@ -291,11 +265,9 @@ def fn_p1s0_tasks():
     global po_hisbase
     global po_sq3exe
     global di_dbs
-
     # Saving the current directory path when program launched
     st_prog_start_dir=str(Path.cwd())
     print(st_prog_start_dir)
-
     # Set workbase directory
     si_zipfile=gv.di_u['st_zipfile_1']
     po_zipfile = Path(si_zipfile)
@@ -304,7 +276,7 @@ def fn_p1s0_tasks():
     print(po_wrkbase)
     po_hisbase = Path(d)/'his'
     print(po_hisbase)
-    
+    # Predefined Variables. Please do not edit the values in this area
     # Entries for creating directories in wb
     li_101_crdir_fmwb = [
       ['p1', 's0', 'e0']
@@ -315,7 +287,6 @@ def fn_p1s0_tasks():
     , ['p6', 's0', 'e0']
     , ['p7', 's0', 'e0']
     ]
-
     # Entries for creating sym inks in wb
     li_102_cdwb_linkstt = [
       ['p1.BDI', 'p1' ]  
@@ -326,10 +297,8 @@ def fn_p1s0_tasks():
     , ['p6.MPL', 'p6' ]
     , ['p7.RQI', 'p7' ]
     ]
-
     for x in li_101_crdir_fmwb:
         Path(po_wrkbase / x[0] / x[1] / x[2]).mkdir(mode=0o755, parents=True, exist_ok=True)
-
     os.chdir(po_wrkbase)
     print(Path.cwd())
     for x in li_102_cdwb_linkstt:
@@ -341,36 +310,25 @@ def fn_p1s0_tasks():
             continue
     os.chdir(st_prog_start_dir)
     print(Path.cwd())
-
     po_eve_scv = po_wrkbase / 'p1' / 's0' / 'e0' / 'events.csv'
     print(po_eve_scv)
-
     po_fls_scv = po_wrkbase / 'p1' / 's0' / 'e0' / 'files.csv'
     print(po_fls_scv)
-
     po_fls_bkp = po_wrkbase / 'p1' / 's0' / 'e0' / 'files.csv.bkp'
     print(po_fls_bkp)
-
     # Define column names of df_files
     cf_files = ['ps_name', 'f_id', 'e_id', 't_id', 'e_name', 'e_type', 
                 'f_name', 'f_size', 's_pct', 'isValid', 'f_modified', 'f_file' ]
-
     cf_files_r = ['ps_name', 'f_id', 'e_id', 't_id', 'e_name', 'e_type', 
                   'f_name', 'f_size', 's_pct', 'isValid', 'f_modified' ]
-    
     cf_events = [ 'fn_name', 'b_time', 'b_remarks', 'e_time', 'e_remarks', 'ela_secs', 'status' ]
-
     # Create dataframes for events
     fn_cr_df_events()
-
     # Create dataframes for file-metadata
     fn_cr_df_files()
-
     # Phase closing tasks
     f_etime_o=datetime.now()
     fn_log_event('fn_setupGround', '', f_btime_o, f_etime_o, 'Setup ground for pipes, end points', 0)
-
-
 def fn_install_p1s1e():
     global p1_s1_e0
     global li_103_p1_s1_e0
@@ -378,14 +336,11 @@ def fn_install_p1s1e():
     li_201_crdir_fmwb = [
       ['p1', 's1', 'e0']
     ]
-
     for x in li_201_crdir_fmwb:
         Path(po_wrkbase / x[0] / x[1] / x[2]).mkdir(mode=0o755, parents=True, exist_ok=True)
-
     # Set the unzip location for extracting the contents of zipfile
     p1_s1_e0 = po_wrkbase / 'p1' / 's1' / 'e0'
     print(p1_s1_e0)
-
     # Provide meta data about input files
     li_103_p1_s1_e0 = [
       ['train_data.csv',          'outage_dna',  'csv' ]
@@ -395,15 +350,11 @@ def fn_install_p1s1e():
     , ['report_data.csv',         't_report',    'csv' ]
     , ['server_data.csv',         't_server',    'csv' ]
     ]
-
-
 def fn_rcvdata_p1s1e():
     f_btime=datetime.now()
     shutil.unpack_archive(po_zipfile, p1_s1_e0)
     f_etime=datetime.now()
     fn_log_event('fn_rcvdata_p1s1e', '', f_btime, f_etime, 'Received data in P1S1E', 0)
-
-
 def fn_chktank_p1s1e(i_inplist):
     print('I am in fn_chktank_p1s1e')
     w_c=datetime.now()
@@ -427,28 +378,20 @@ def fn_chktank_p1s1e(i_inplist):
     fn_cc_df_files()
     fn_show_df_files('p1s1')
     print('I am out fn_chktank_p1s1e')
-
-
 def fn_p1s1_tasks():
     print('I am in fn_p1s1_tasks')
     f_btime_o=datetime.now()
-    
     # Create pipe p1s1 for receiving data
     fn_install_p1s1e()
-    
     # Receive data from Input_Zipfile to P1S1E
     fn_rcvdata_p1s1e()
-
     # Check details of the data received
     fn_chktank_p1s1e(li_103_p1_s1_e0)
-
     # Phase closing tasks
     f_etime_o=datetime.now()
     fn_log_event('fn_irc_p1s1e', '', f_btime_o, f_etime_o, 'Install, Receive and Check P1S1E', 0)
     fn_closing_event('p1s1','N')
     print('I am out fn_p1s1_tasks')
-
-
 def fn_copy_df_files(i_src_pname, i_src_sname, i_tgt_pname, i_tgt_sname):
     i_src_psname = i_src_pname + i_src_sname
     i_tgt_psname = i_tgt_pname + i_tgt_sname
@@ -465,8 +408,6 @@ def fn_copy_df_files(i_src_pname, i_src_sname, i_tgt_pname, i_tgt_sname):
         j=j+1
     fn_cc_df_files()
     fn_show_df_files(i_tgt_psname)
-
-
 def fn_install_p1s2e():
     # Entries for creating directories in wb
     li_301_crdir_fmwb = [
@@ -478,14 +419,10 @@ def fn_install_p1s2e():
     , ['p1', 's2', 'e6']
     , ['p1', 's2', 'e7']
     ]
-
     for x in li_301_crdir_fmwb:
         Path(po_wrkbase / x[0] / x[1] / x[2]).mkdir(mode=0o755, parents=True, exist_ok=True)
-
     # Layout P1S2 pipes
     fn_copy_df_files('p1', 's1', 'p1', 's2')
-
-
 def fn_rcvdata_p1s2e():
     df1 = df_files.iloc[np.where(df_files.ps_name == 'p1s1')]
     df2 = df_files.iloc[np.where(df_files.ps_name == 'p1s2')]
@@ -493,22 +430,18 @@ def fn_rcvdata_p1s2e():
     df4=df3.iloc[:, [1,2,5,6,17,11,22]]
     df5a = df4.iloc[np.where(df4.e_type_x == 'csv')]
     df5b = df4.iloc[np.where(df4.e_type_x == 'txt')]
-
     for x in df5a.index:
         s_f=df5a.loc[x][5]
         t_f=df5a.loc[x][6]
         fv_src = Path(s_f)
         fv_tgt = Path(t_f)
-
         fv_src.rename(fv_tgt)
         print('\nMoving {}\n    to {}'.format(s_f,t_f))
-        
     for x in df5b.index:
         s_f=df5b.loc[x][5]
         t_f=df5b.loc[x][6]
         fv_src = Path(s_f)
         fv_tgt = Path(t_f)
-        
         sf = open(fv_src, 'r')
         tf = open(fv_tgt, 'w')
         Lines = sf.readlines()
@@ -517,10 +450,7 @@ def fn_rcvdata_p1s2e():
             tf.writelines(new_line)
         sf.close()
         tf.close()
-        
         print('\nModify {}\n    to {}'.format(s_f,t_f))
-
-
 def fn_chktank_p1s2e():
     for x in df_files.index:
         v_1=df_files.iloc[x][0]
@@ -532,48 +462,35 @@ def fn_chktank_p1s2e():
             df_files.iloc[x,5]='csv'
             print(df_files.iloc[x])
     fn_show_df_files('p1s2')
-
-
 def fn_p1s2_tasks():
     # Opening tasks for this phase
     f_btime_o=datetime.now()
     fn_restart_df_files()
     fn_show_df_files('p1s1')  
-
     # Create pipe p1s2 for receiving data
     fn_install_p1s2e()
-    
     # Receive data from P1S1E to P1S2E
     fn_rcvdata_p1s2e()
-
     # Check details of the data received
     fn_chktank_p1s2e()
-
     # Closing tasks for this phase
     f_etime_o=datetime.now()
     fn_log_event('fn_irc_p1s2e', '', f_btime_o, f_etime_o, 'Install, Receive and Check P1S2E', 0)
     fn_closing_event('p1s2','N')
-
-
 def fn_install_p1s3e():
     fn_setup_sqlite3()
-    
     # Create information repository for this project
     fn_create_sq3dbf('zz1', 'p1', 's0')
     fn_cc_df_files()
     fn_cc_df_events()
     fn_show_df_files('p1s0')
-    
     # Creae the database for this phase
     fn_create_sq3dbf('db0', 'p1', 's3')
     fn_cc_df_files()
     fn_cc_df_events()
     fn_show_df_files('p1s3')
-
-
 def fn_rcvdata_p1s3e(i_sqlid):
     f_btime_o=datetime.now()
-
     for x in df_files.iloc[np.where((df_files.e_type=='sq3ex')&(df_files.e_name=='exe0'))].loc[:,'f_file']:
         ff_exe=str(x)
     for x in df_files.iloc[np.where((df_files.e_type=='sq3in')&(df_files.e_name=='inp0') 
@@ -583,12 +500,9 @@ def fn_rcvdata_p1s3e(i_sqlid):
                                     & (df_files.t_id==1))].loc[:,'f_file']:
         ff_out=str(x)
     f_run = '"' + ff_exe + '" < "' + ff_inp + '"' + ' > "' + ff_out + '"'
-
     get_sqlstr(i_sqlid)
-   
     fli_1=df_files.loc[np.where(df_files.ps_name=='p1s2')
                         ].loc[:,['e_name', 't_id', 'f_file' ]].values.tolist()
-
     for x in fli_1:
         fe_nm, ft_id, ff_fl = x
         mesg='\nImporting {sv_01}\n       to TABLE {sv_02}_{sv_03:02}'
@@ -613,8 +527,6 @@ def fn_rcvdata_p1s3e(i_sqlid):
     fn_show_df_files('p1s3')
     f_etime_o=datetime.now()
     fn_log_event('fn_rcvdata_p1s3e', '', f_btime_o, f_etime_o, 'Received data in P1S3E', 0)
-
-
 def fn_chktank_p1s3e(i_sqlid_a,i_sqlid_b,i_sqlid_c):
     f_btime_o=datetime.now()
     for x in df_files.iloc[np.where((df_files.e_type=='sq3ex')&(df_files.e_name=='exe0'))].loc[:,'f_file']:
@@ -641,9 +553,7 @@ def fn_chktank_p1s3e(i_sqlid_a,i_sqlid_b,i_sqlid_c):
     fn_log_event('fn_execsql', '', fi_btime, fi_etime, f_tb, f_rc)
     with open(ff_out, 'r') as f:
         print(f.read())
-    
     get_sqlstr(i_sqlid_b)
-
     fli_1=df_files.loc[np.where(df_files.ps_name=='p1s2')
                         ].loc[:,['e_name', 't_id', 'f_file' ]].values.tolist()
     for x in fli_1:
@@ -674,33 +584,25 @@ def fn_chktank_p1s3e(i_sqlid_a,i_sqlid_b,i_sqlid_c):
     fn_log_event('fn_execsql', '', fi_btime, fi_etime, f_tb, f_rc)
     with open(ff_out, 'r') as f:
         print(f.read())
-
     fn_cc_df_files()
     fn_cc_df_events()
     fn_show_df_files('p1s3')
     f_etime_o=datetime.now()
     fn_log_event('fn_chktank_p1s3e', '', f_btime_o, f_etime_o, 'Checked data in P1S3E', 0)
     do_ckpt_type_a()
-
-
 def fn_p1s3_tasks():
     # Opening tasks for this phase
     f_btime_o=datetime.now()
     fn_restart_df_files()
     fn_show_df_files('p1s2')  
     fn_create_di_dbs()
-    
     # Create pipe p1s3 for receiving data
     fn_install_p1s3e()
-    
     # Receive data from P1S2E to P1S3E
     fn_rcvdata_p1s3e(122)
-
     # Check details of the data received
     fn_chktank_p1s3e(131,132,133)
-        
     # Closing tasks for this phase
     f_etime_o=datetime.now()
     fn_log_event('fn_irc_p1s3e', '', f_btime_o, f_etime_o, 'Install, Receive and Check P1S3E', 0)
     fn_closing_event('p1s3')
-
